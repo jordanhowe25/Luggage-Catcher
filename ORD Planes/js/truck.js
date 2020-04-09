bcbec55df6bb04b6e886f0eb41f10e8c820269b0
Jordan Howe <jordanhowe25@gmail.com>
2020-04-09 10:10:27 -0500

    Updating Class Files
    For some reason I missed a few class files that were supposed to be in repo.  These are now added

---
+++ ORD Planes/js/truck.js
@@ -0,0 +1,50 @@
+/*
+// sets the cat's image
+var catImage = new Image();
+catImage.src = './images/cat.png';
+
+
+// draws cat 
+
+function drawCat(){
+  ctx.drawImage(catImage,cat.x,cat.y, cat.width, cat.height)
+}
+
+
+// controls movement
+document.onkeydown = function (e) {
+  console.log("working moving!!!!");
+  if (e.which === 38){
+    cat.y -= 20;
+  }
+  else if (e.which === 40)
+  {
+    cat.y += 20;
+  }
+  else if (e.which === 37){
+    cat.x -= 20;
+  }
+  else if (e.which === 39){
+    cat.x += 20;
+  }
+}
+  
+ // collision detection, set y property to any number greater than 1000 in order to make the object disappear
+
+function cupcakeCollision() {
+  cupCakesArray.forEach(function(cupcake) {
+    let catLeft = cat.x;
+    let catRight = cat.x + cat.width/2;
+    let catTop = cat.y;
+    let catBottom = cat.y + cat.height/2;
+    let cupcakeLeft = cupcake.x;
+    let cupcakeRight = cupcake.x + cupcake.width/2;
+    let cupcakeTop = cupcake.y;
+    let cupcakeBottom = cupcake.y + cupcake.height;
+    if(catLeft <= cupcakeRight && cupcakeLeft <= catRight && catBottom <= cupcakeBottom  && cupcakeTop <= catBottom){
+      cupcake.y = 1444;
+      AddPoints(10);
+    }
+    })
+}
+*/
