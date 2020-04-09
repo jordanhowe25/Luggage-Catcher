691f6ba486e6a11aab9b4e4d130f0ab4e6b2f27a
Jordan Howe <32051012+jordanhowe25@users.noreply.github.com>
2020-04-09 01:24:44 -0500

    Add files via upload

---
+++ ORD Planes/js/game.js
@@ -0,0 +1,130 @@
+//global vars
+var canvasWidth = (window.innerWidth * .7);
+var canvasHeight = (window.innerHeight * .7);
+
+//Create Canvas
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+/*This is just a test to show that the height and width are set for the canvas properly.  
+You should see small red border around canvas.*/
+ctx.strokeStyle = "#FF0000";
+ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+
+
+/*
+// set width and height for cat variable
+
+var cat = {
+  x: 600,
+  y: 500,
+  width: 150,
+  height:150
+}
+
+
+updateCanvas(); 
+
+
+// scoring functionality, Calling $('#id') will return a jQuery object that wraps the DOM object and provides jQuery methods., .html is A function returning the HTML content to set and returns as String
+
+var score = 0;
+function ShowScore(){
+  $(".score").html(score);
+
+}
+function AddPoints(pointsToAdd){
+  score += pointsToAdd;
+}
+function LosePoints(pointsLost){
+  score -= pointsLost;
+}
+
+// if (score < 0){
+//   alert("Game Over");
+// }
+
+// sets the cat's image
+var catImage = new Image();
+catImage.src = './images/cat.png';
+
+
+// draws cat 
+
+function drawCat(){
+  ctx.drawImage(catImage,cat.x,cat.y, cat.width, cat.height)
+}
+
+
+// controls movement
+document.onkeydown = function (e) {
+  console.log("working moving!!!!");
+  if (e.which === 38){
+    cat.y -= 20;
+  }
+  else if (e.which === 40)
+  {
+    cat.y += 20;
+  }
+  else if (e.which === 37){
+    cat.x -= 20;
+  }
+  else if (e.which === 39){
+    cat.x += 20;
+  }
+}
+  
+ // collision detection, set y property to any number greater than 1000 in order to make the object disappear
+
+function cupcakeCollision() {
+  cupCakesArray.forEach(function(cupcake) {
+    let catLeft = cat.x;
+    let catRight = cat.x + cat.width/2;
+    let catTop = cat.y;
+    let catBottom = cat.y + cat.height/2;
+    let cupcakeLeft = cupcake.x;
+    let cupcakeRight = cupcake.x + cupcake.width/2;
+    let cupcakeTop = cupcake.y;
+    let cupcakeBottom = cupcake.y + cupcake.height;
+    if(catLeft <= cupcakeRight && cupcakeLeft <= catRight && catBottom <= cupcakeBottom  && cupcakeTop <= catBottom){
+      cupcake.y = 1444;
+      AddPoints(10);
+    }
+    })
+}
+
+function shardCollision() {
+  shardsArray.forEach(function(shard) {
+    let catLeft = cat.x;
+    let catRight = cat.x + cat.width/2;
+    let catTop = cat.y;
+    let catBottom = cat.y + cat.height/2;
+    let shardLeft = shard.x;
+    let shardRight = shard.x + shard.width/2;
+    let shardTop = shard.y;
+    let shardBottom = shard.y + shard.height;
+    if(catLeft <= shardRight && shardLeft <= catRight && catBottom <= shardBottom  && shardTop <= catBottom){
+      shard.y = 1444;
+      LosePoints(10);
+    }
+  });
+}
+  
+ 
+
+
+function updateCanvas(){
+  setInterval(function(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCat();
+    drawAllTheCupcakes();
+    drawAllTheShards();
+    shardCollision();
+    cupcakeCollision();
+    ShowScore();
+  },100)
+}
+*/
