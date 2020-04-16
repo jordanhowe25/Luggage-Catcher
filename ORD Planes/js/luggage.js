0073345b207ee89020a4a82df1cb13fbd8517686
Lukiinator <lucasagruber@yahoo.com>
2020-04-16 11:56:34 -0400

    Update luggage.js

--- ORD Planes/js/luggage.js
+++ ORD Planes/js/luggage.js
@@ -1,12 +1,13 @@
 
-var cupCakesArray = [];
-var CrateImageOne = new Image();
+// Declare Variables
+var luggageArray = [];
+var luggageImageOne = new Image();
 CrateImageOne.src = "./images/LuggageCrate1.png";
-var CrateImageTwo = new Image();
+var luggageImageTwo = new Image();
 CrateImageTwo.src = "./images/LuggageCrate2.png";
-var CrateImageThree = new Image();
+var luggageImageThree = new Image();
 CrateImageThree = "./images/LuggageCrate3.png";
-var CrateImageFour = new Image();
+var luggageImageFour = new Image();
 CrateImageFour = "./images/LuggageCrate4.png";
 infiniteLuggage();
 
@@ -14,39 +15,39 @@ function infiniteLuggage (){
     var luggageIndex = 0;
     setInterval(function(){
         addLuggage();
-        fallingLuggage(cupCakesArray[luggageIndex]);
+        fallingLuggage(luggageArray[luggageIndex]);
         luggageIndex++;
     }, 2000)
 }
 
-// var theCupCake = {y: 0, x: 0 };
+var theLuggage = {y: 0, x: 0 };
 
 function addLuggage() {
     var rando = Math.floor(Math.random() * 1000);
-    theCupCake = {
+    theLuggage = {
         x: 0, 
         y: 0, 
         width:50,
         height:50 
     };
-    theCupCake.x = rando;
-    cupCakesArray.push(theCupCake);
+    theLuggage.x = rando;
+    luggageArray.push(theLuggage);
 }
 
 
-function fallingLuggage (cupcake){
+function fallingLuggage (luggage){
     var id = setInterval(function(){
-        cupcake.y+=8
-        if(cupcake.y > 1000){
+        luggage.y+=8
+        if(luggage.y > 1000){
             clearInterval(id)
         }
     },100)
 }
 
-function drawAllTheCupcakes(){
+function drawAllTheLuggage(){
     //  console.log("falling");
-   cupCakesArray.forEach(function(oneCupcake){
-        ctx.drawImage(CrateImageOne,oneCupcake.x, oneCupcake.y,oneCupcake.width, oneCupcake.height)
+   luggageArray.forEach(function(oneLuggage){
+        ctx.drawImage(luggageImageOne,oneLuggage.x, oneLuggage.y,oneLuggage.width, oneLuggage.height)
    }) 
 
 }
