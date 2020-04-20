a82bda30d92bed45d7a5e8f4032c3199172ecdbd
Gonzalo Montoya <Gonzalo413@users.noreply.github.com>
2020-04-20 05:55:59 -0500

    updated countdown 1
    timer is counting down

--- ORD Planes/js/game.js
+++ ORD Planes/js/game.js
@@ -27,6 +27,21 @@ function updateCanvas(){
   },10)
 }
 
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() 
+{
+
+const minutes = Math.floor(time/60);
+let seconds = time % 60; 
+
+seconds = seconds <10? '0' + seconds:seconds;
+
+countdownEl.innerHTML = `${minutes}:${seconds}`;
+  time--;
+}
+
+
 function startGame(){
   hideStartWindow();
   truck = new Truck({});
