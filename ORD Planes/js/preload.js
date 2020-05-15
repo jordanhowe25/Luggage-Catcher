6ab17873a7919c5b72795f5aebd4feb317ba353e
Jordan Howe <jordanhowe25@gmail.com>
2020-05-14 22:57:40 -0500

    Added Timeout Feature

--- ORD Planes/js/preload.js
+++ ORD Planes/js/preload.js
@@ -17,3 +17,22 @@
         return img;  
     })
 })();
+
+(() => {
+    var t;
+    //window.onload = resetTimer;
+    window.onmousemove = resetTimer; // catches mouse movements
+    window.onmousedown = resetTimer; // catches mouse movements
+    window.onclick = resetTimer;     // catches mouse clicks
+    window.onscroll = resetTimer;    // catches scrolling
+    window.onkeypress = resetTimer;  //catches keyboard actions
+ 
+   function reload() {
+          window.location.href = 'http://www.flywithbutchohare.com';  
+   }
+ 
+   function resetTimer() {
+        clearTimeout(t);
+        t= setTimeout(reload, 120000);  // time is in milliseconds (1000 is 1 second)
+    }   
+})();
