20078afd2b86d2c11d938674589e08bfbc11fea8
Jordan Howe <jordanhowe25@gmail.com>
2020-06-29 22:47:03 -0500

    Update

---
+++ docs/js/game.js
@@ -0,0 +1,201 @@
+/* globals Truck Luggage Plane */
+
+class Game {
+  constructor({
+    canvasElementId,
+    onScoreUpdate = (/* score */) => {},
+    luggageSpawnInterval = 333,
+  }){
+    this._score = 0;
+    this._luggage = new Set();
+    this._canvasElementId = canvasElementId;
+    this._onScoreUpdate = onScoreUpdate;
+    this._luggageSpawnInterval = luggageSpawnInterval;
+    this._time = 180;
+    this._endGame = false;
+  
+    this.createCanvas(canvasElementId);
+  }
+
+  get canvas(){ return this._canvas; }
+  set canvas(value){ this._canvas = value; }
+
+  get canvasWidth(){ return 1400; }
+  get canvasHeight(){ return 700; }
+
+  get canvasElementId(){ return this._canvasElementId; }
+
+  get onScoreUpdate(){ return this._onScoreUpdate; }
+
+  get luggageSpawnInterval() { return this._luggageSpawnInterval; }
+
+  get luggageSpawner() { return this._luggageSpawner; }
+  set luggageSpawner(val) { this._luggageSpawner = val; }
+
+  get score(){ return this._score; }
+  set score(value){ this._score = value; }
+
+  get truck(){ return this._truck; }
+  set truck(value) { this._truck = value; }
+
+  get luggage(){ return this._luggage; }
+
+  get plane(){ return this._plane; }
+  set plane(value) { this._plane = value; }
+
+  get timer() { return this._timer; }
+  set timer(value) { this._timer = value; }
+  
+  get time(){ return this._time; }
+  set time(value){ this._time = value; }
+
+  get endGame() { return this._endGame; }
+  set endGame(value){ this._endGame = value; }
+
+
+
+  createCanvas(elementId){ 
+    const canvas = document.getElementById(elementId);
+    this.canvas = canvas.getContext('2d');
+    canvas.width = this.canvasWidth;
+    canvas.height = this.canvasHeight;
+
+    return this.canvas;
+  }
+
+  clearCanvas(){
+    this.canvas.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  
+  startCountdown()
+  {
+    this.timer = setInterval(() => {
+      var seconds = this.time;
+      const minutes = Math.floor(seconds/60);
+      const time001 = document.getElementById('time001');
+      seconds = seconds % 60;
+      seconds = seconds <10? '0' + seconds:seconds;
+      time001.innerHTML = minutes + ':' + seconds;
+      this.time --;
+      if (this.time < 0) {
+        this.stopCountdown();
+        this.stop();
+      }
+    }, 1000);
+  }
+
+  stopCountdown() {
+    clearInterval(this.timer);
+  }
+
+  updateScore(int = 0) {
+    this.score += int;
+    this.onScoreUpdate(this.score)
+  }
+
+  moveX(int) {
+    this.truck.move(int);
+  }
+
+  spawnTruck() {
+   this.truck = new Truck({
+     game: this,
+   });
+  }
+
+  clearTruck() {
+    this.truck.stopAnimation();
+  }
+
+  spawnPlane() {
+    this.plane = new Plane({
+      game: this,
+    });
+  }
+
+  clearPlane() {
+    this.plane.stopAnimation();
+  }
+
+  stopLuggageSpawner() {
+    clearInterval(this.luggageSpawner);
+    this.luggageSpawner = null;
+  }
+
+  startLuggageSpawner() {
+    this.luggageSpawner = setInterval(
+      () => this.spawnLuggage(), 
+      this.luggageSpawnInterval
+    );
+  }
+
+  // spawn luggage relative to plane
+  spawnLuggage() {
+    const {x, y, width, height} = this.plane;
+    const offsetX = 15;
+    const offsetY = 10;
+    const triggerSpawn = Math.random() >= 0.5;
+
+    // only spawn luggage if plane is on screen
+    if (this.plane.x >= 0 && this.plane.x + this.plane.width + 25 <= this.canvasWidth) {
+      //50-50 chance that the interval will create luggage. Adds some randomness.
+      if (triggerSpawn) {
+        // spawn some luggage
+        this.luggage.add(
+          new Luggage({
+            x: (x + width - offsetX),
+            y: (y + height + - offsetY),
+            game: this,
+          })
+        );
+      }
+    }
+  }
+
+  start() {
+    this.endGame = false;
+    this.score = 0;
+    this.time = 180;
+    this.startCountdown();
+    this.updateScore(0, () => {});
+    this.spawnTruck();
+    this.spawnPlane();
+    this.startLuggageSpawner();
+  }
+
+  stop() {
+    this.stopCountdown();
+    this.stopLuggageSpawner();
+    $('.end-score').text(this.score);
+    this.endGame = true;
+    this.clearPlane();
+    this.clearTruck();
+    this.truck = null;
+    this.plane = null;
+    
+    $('#game-scene').fadeToggle();
+    $('#end-screen-window').fadeToggle();
+    $('#small-logo').fadeToggle();
+  }
+
+  // return true if two bounding boxes collided
+  static didCollide(obj1, obj2){
+    // since we have getBoundingBox it should be easy
+    if (!obj1.getBoundingBox && !obj2.getBoundingBox) return false;
+
+    // get boxes
+    const box1 = obj1.getBoundingBox();
+    const box2 = obj2.getBoundingBox();
+
+    // check for intersections based on the vertices from bounding box
+    return (box1.topLeft.x < (box2.bottomRight.x) && 
+    (box1.bottomRight.x) > box2.topLeft.x) && 
+    (box1.topLeft.y < (box2.bottomRight.y) && 
+    (box1.bottomRight.y) > box2.topLeft.y);
+  }
+}
+
+const game = new Game({
+  canvasElementId: 'canvas',
+  onScoreUpdate: (score) => $('.score').text(score), 
+});
---
+++ docs/js/luggage.js
@@ -0,0 +1,132 @@
+
+/* globals Game */
+
+class Luggage {
+    constructor({
+      x = 0,
+      y = 0,
+      height = 50,
+      width = 50,
+      images = [
+          './images/luggage-blue.png',
+          './images/luggage-pink.png',
+          './images/luggage-yellow.png',
+          './images/luggage-red.png'
+      ],
+      maxSpeed = 4,
+      game,
+    }) {
+      // position data
+      this._x = x;
+      this._y = y;
+      this._height = height;
+      this._width = width;
+      this._maxSpeed = maxSpeed;
+  
+  
+      // parent game
+      this._game = game;
+  
+      // images to use
+      this._images = this.loadImages(images);
+  
+      // loadImage images
+      this.loadImage();
+  
+      // set random speed for luggage based on maxSpeed
+      this._speed = Math.floor(Math.random() * (maxSpeed - 1) + 1);
+  
+      // draw to canvas at intial position
+      this.draw();
+  
+      // start animation lifecycle
+      this.startAnimation();
+    }
+  
+    // Getters and Setters
+    get game(){ return this._game; }
+    get x() { return this._x; }
+    set x(x) { this._x = x; }
+    get y() { return this._y; }
+    set y(y) { this._y = y; }
+    get speed() { return this._speed; }
+    set speed(speed) { this._speed = speed; }
+    get maxSpeed() { return this._maxSpeed; }
+    get height() { return this._height; }
+    get width() { return this._width; }
+    get canvas() { return this.game.canvas; }
+    get images() { return this._images; }
+    get image() { return this._image; }
+    set image(src) { this._image = src; }
+    get animationInterval() { return this._animationInterval; }
+    set animationInterval(interval) { this._animationInterval = interval }
+  
+    loadImages(images) {
+      return images.map(src => {
+        const img = new Image();
+        img.height = this.height;
+        img.width = this.width;
+        img.src = src;
+        return img;
+      });
+    }
+      
+    loadImage() {
+      this.image = this.images[Math.floor(Math.random() * this.images.length)];
+    }
+      
+    update() {
+      this.clear();
+      this.y += this.speed;
+      this.draw();
+    }
+  
+    draw() {
+      this.canvas.drawImage(
+        this.image,
+        this.x, 
+        this.y,
+        this.width, 
+        this.height
+      );
+    }
+  
+    clear() {
+      // remove object from canvas 
+      this.canvas.clearRect(
+        this.x,
+        this.y,
+        this.width,
+        this.height,
+      );
+    }
+  
+    startAnimation() {
+      const request = requestAnimationFrame(() => this.startAnimation(request));
+      if (this.y >= 560 || game.endGame == true){
+        // collided with bottom of screen -- no points
+        this.clear();
+        cancelAnimationFrame(request);
+      } else {
+        this.update();
+        // check for collision with truck -- add points
+        if(Game.didCollide(this.game.truck, this)) {
+          this.clear();
+          // remove object from game memory
+          this.game.luggage.delete(this);
+          this.game.updateScore(1);
+          cancelAnimationFrame(request);
+        }
+      }
+    }
+  
+    // return vertices of boundingbox
+    getBoundingBox() {
+      return {
+        topLeft: {x: this.x, y: this.y},
+        bottomRight: {x: this.x + this.width, y: this.y + this.height},
+      }
+    }
+  };
+  
+
